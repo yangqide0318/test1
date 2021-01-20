6c24ca330f2ecc001727677bd89554febe86c993
yangqide0318 <3162426270@qq.com>
2021-01-20 16:55:39 +0800

    *

---
+++ readme.txt
@@ -0,0 +1,10 @@
+login.html 登录页面 核心 login.js
+
+index.html 首页  核心  department.js
+
+department.html 部门  核心  department.js
+    1.添加部门 btnAdd按钮
+
+
+employeeManagement.html 员工
+positionManagement.html 职位
---
+++ static/js/index.js
@@ -0,0 +1,49 @@
+$("#schManagement").on("click", function () {
+    // $.ajaxSetup({cache: false});
+    $(".main-container").load("SchoolManagement.html");
+});
+$("#courierManagement").on("click", function () {
+    // $.ajaxSetup({cache: false});
+    $(".main-container").load("courierManagement.html");
+});
+$("#classManagement").on("click", function () {
+    // $.ajaxSetup({cache: false});
+    $(".main-container").load("classManagement.html");
+});
+$("#stuManagement").on("click", function () {
+    // $.ajaxSetup({cache: false});
+    $(".main-container").load("studentManagement.html");
+});
+$("#positionManagement").on("click", function () {
+    // $.ajaxSetup({cache: false});
+    $(".main-container").load("positionManagement.html");
+});
+$("#depManagement").on("click", function () {
+    // $.ajaxSetup({cache: false});
+    $(".main-container").load("department.html");
+});
+$("#employeeManagement").on("click", function () {
+    // $.ajaxSetup({cache: false});
+    $(".main-container").load("../frame/employeeManagement.html");
+});
+$("#orderManagement").on("click", function () {
+    // $.ajaxSetup({cache: false});
+    $(".main-container").load("../frame/orderManagement.html");
+});
+
+$("#menuManagement").on("click", function () {
+    // $.ajaxSetup({cache: false});
+    $(".main-container").load("../frame/menuManagement.html");
+});
+$("#areaCounts").on("click", function () {
+    // $.ajaxSetup({cache: false});
+    $(".main-container").load("areaCounts.html");
+});
+$("#schoolCounts").on("click", function () {
+    // $.ajaxSetup({cache: false});
+    $(".main-container").load("schoolCounts.html");
+});
+$("#schoolActManagement").on("click", function () {
+    // $.ajaxSetup({cache: false});
+    $(".main-container").load("schoolActManagement.html");
+});
